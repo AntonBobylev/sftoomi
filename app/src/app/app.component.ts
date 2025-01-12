885cb7780a1766634cd6dbfa68adfda65ba9436f
AntonBobylev <ya-anton3632@yandex.ru>
2025-01-13 00:27:31 +0300

    layout components added

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './components/layout/header/header.component';
+import { ContentComponent } from './components/layout/content/content.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+
 @Component({
     selector: 'app-root',
-    imports: [],
+    imports: [
+        HeaderComponent,
+        ContentComponent,
+        FooterComponent
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
