0e4e01f5105e5ea8bcd497d06985fe314e155d79
AntonBobylev <ya-anton3632@yandex.ru>
2025-01-27 21:54:10 +0300

    base nav panel added

--- app/src/app/components/layout/header/header.component.ts
+++ app/src/app/components/layout/header/header.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { TuiButton } from '@taiga-ui/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    imports: [],
+    imports: [
+        TuiButton,
+        RouterLink
+    ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
