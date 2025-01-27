c021fb660295b3f2f65fe92eddc9ebf00a1ca763
AntonBobylev <ya-anton3632@yandex.ru>
2025-01-27 21:24:01 +0300

    patients module component added

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './components/views/home/home.component';
+import { PatientsComponent } from './components/views/patients/patients.component';
 
 export const routes: Routes = [{
     path: '',
     title: 'Home',
     component: HomeComponent
+}, {
+    path: 'patients',
+    title: 'Patients',
+    component: PatientsComponent
 }];
