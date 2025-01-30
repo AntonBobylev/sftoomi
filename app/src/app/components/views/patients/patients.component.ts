55406cc8ac7f68c302ae7351bab1271b73987f9c
AntonBobylev <ya-anton3632@yandex.ru>
2025-01-30 16:01:43 +0300

    patients table added (front)

--- app/src/app/components/views/patients/patients.component.ts
+++ app/src/app/components/views/patients/patients.component.ts
@@ -1,22 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import {
+    TuiTableCell,
+    TuiTableDirective,
+    TuiTableTbody, TuiTableTd,
+    TuiTableTh,
+    TuiTableThGroup,
+    TuiTableTr
+} from '@taiga-ui/addon-table';
+import { NgForOf } from '@angular/common';
+import { TuiLoader } from '@taiga-ui/core';
 
 import Fetcher from '../../../class/Fetcher';
+import Sftoomi from '../../../class/Sftoomi';
+
+import getPatientsAPI from '../../../APIs/getPatientsAPI';
 
 @Component({
     selector: 'app-patients',
-    imports: [],
+    imports: [
+        TuiTableDirective, TuiTableThGroup, TuiTableTh,
+        TuiTableTbody, NgForOf, TuiTableTr, TuiTableCell,
+        TuiTableTd, TuiLoader
+    ],
     templateUrl: './patients.component.html',
     styleUrl: './patients.component.scss'
 })
 
 export class PatientsComponent implements OnInit
 {
+    protected readonly Sftoomi = Sftoomi;
+
+    protected readonly data: WritableSignal<getPatientsAPI['data']> = signal<getPatientsAPI['data']>([]);
+    protected readonly isLoading: WritableSignal<boolean> = signal<boolean>(false);
+
+    protected readonly columns: string[] = [
+        'id',
+        'last_name',
+        'first_name',
+        'middle_name',
+        'dob'
+    ];
+
     ngOnInit(): void
     {
+        let me: this = this;
+        me.isLoading.set(true);
+
         (new Fetcher).request({
-            url: 'http://localhost:8080/patients',
-            success: function (response: any, request: any, data: any): void {
-                console.log('API works!');
+            url: 'http://localhost:8080/getPatients',
+            success: function (response: any, request: XMLHttpRequest, data: getPatientsAPI): void {
+                me.isLoading.set(false);
+                me.data.set(data.data);
+            },
+            failure: function (): void {
+                me.isLoading.set(false);
             }
         })
     }
