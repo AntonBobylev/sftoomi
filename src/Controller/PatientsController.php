6d0f74d7e5e6b1885a78546c9c4deddc2ae40137
AntonBobylev <ya-anton3632@yandex.ru>
2025-01-30 14:30:31 +0300

    getPatients API implemented

--- src/Controller/PatientsController.php
+++ src/Controller/PatientsController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\DataMappers\PatientDM;
+use App\Repository\PatientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class PatientsController
+class PatientsController extends AbstractController
 {
-    #[Route('/patients', name: 'get_patients')]
-    public function getPatients(): Response
+    #[Route('/getPatients', name: 'get_patients')]
+    public function getPatients(PatientRepository $patientRepository): Response
     {
+        $patientsEntities = $patientRepository->findAll();
+
+        $patients = [];
+        foreach ($patientsEntities as $patientEntity) {
+            $patients[] = (new PatientDM)->entityToData($patientEntity);
+        }
+
         return new JsonResponse([
-            "test" => "test"
+            "data" => $patients
         ]);
     }
 }
