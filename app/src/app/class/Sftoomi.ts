2ee673743f911b381b8949c99c254223cbac6f5a
AntonBobylev <ya-anton3632@yandex.ru>
2025-01-30 13:17:25 +0300

    new methods in Sftoomi class

--- app/src/app/class/Sftoomi.ts
+++ app/src/app/class/Sftoomi.ts
@@ -1,7 +1,41 @@
+import moment from 'moment';
+
 export default class Sftoomi
 {
+    /**
+     * Returns current year number
+     */
     public static getCurrentYear(): number
     {
         return (new Date()).getFullYear();
+    }
+
+    /**
+     * Checks is variable an array
+     *
+     * @param variable
+     */
+    public static isArray(variable: any): boolean
+    {
+        return toString.apply(variable) === '[object Array]';
+    }
+
+    /**
+     * Checks is variable a date
+     *
+     * @param variable
+     */
+    public static isDate(variable: any): boolean
+    {
+        return variable instanceof Date;
+    }
+
+    public static dateShort(date?: string | null | Date): string | null
+    {
+        if (!date) {
+            return '';
+        }
+
+        return moment(date).format('MM/DD/YYYY');
     }
 }
