ddd96e8bb2049abc6a1d50d2cd50ce4a5069523b
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-16 17:33:00 +0300

    Cookies service added

---
+++ app/src/app/services/cookies-service.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+import SftoomiCookie from '../enumerations/SftoomiCookies.enumeration';
+
+@Injectable()
+export class CookiesService
+{
+    public deleteCookie(name: SftoomiCookie): void
+    {
+        this.setCookie(name, '', -1);
+    }
+
+    public getCookie(name: SftoomiCookie): string
+    {
+        const cookies: Array<string> = decodeURIComponent(document.cookie).split(';'),
+            cookiesLen: number = cookies.length,
+            cookieName: string = `${name}=`;
+
+        for (let i: number = 0; i < cookiesLen; i += 1) {
+            let currentCookie: string = cookies[i].replace(/^\s+/g, '');
+
+            if (currentCookie.indexOf(cookieName) === 0) {
+                return currentCookie.substring(cookieName.length, currentCookie.length);
+            }
+        }
+
+        return '';
+    }
+
+    public setCookie(name: SftoomiCookie, value: string, expireDays: number, path: string = ''): void
+    {
+        const d: Date = new Date();
+        d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+
+        const expires: string = `expires=${d.toUTCString()}`,
+            cpath: string = path ? `; path=${path}` : '';
+
+        document.cookie = `${name}=${value}; ${expires}${cpath}`;
+    }
+}
