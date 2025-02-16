11babbf4be28b775734dc3b3918977feb2596ae0
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-16 17:35:08 +0300

    app-language-switcher added to header

--- app/src/app/components/layout/header/header.component.ts
+++ app/src/app/components/layout/header/header.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { TuiButton } from '@taiga-ui/core';
 import { RouterLink } from '@angular/router';
 
+import Sftoomi from '../../../class/Sftoomi';
+
+import I18nSwitcherComponent from '../../core/app-language-switcher/app-language-switcher.component';
+
 @Component({
     selector: 'app-header',
     imports: [
         TuiButton,
-        RouterLink
+        RouterLink,
+        I18nSwitcherComponent
     ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
@@ -15,4 +20,5 @@ import { RouterLink } from '@angular/router';
 export class HeaderComponent
 {
 
+    protected readonly Sftoomi = Sftoomi;
 }
