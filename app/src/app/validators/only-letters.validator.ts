254bed516f50a7f1e4ac076708aaf9c2d2b685b5
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-16 14:26:59 +0300

    only letters validator added

---
+++ app/src/app/validators/only-letters.validator.ts
@@ -0,0 +1,14 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function onlyLettersValidator(): ValidatorFn
+{
+    return (control: AbstractControl) : ValidationErrors | null => {
+
+        const valid = /^[a-zA-ZА-я]*$/.test(control.value);
+
+        return valid
+            ? null
+            : {only_letters: {value: control.value}};
+    }
+
+}
