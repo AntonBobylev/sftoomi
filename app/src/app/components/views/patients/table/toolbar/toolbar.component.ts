3aa872dcf0c34187ebb67841f7b495102c6ca5b1
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-16 13:57:53 +0300

    PatientEditDialog opening added for toolbar

--- app/src/app/components/views/patients/table/toolbar/toolbar.component.ts
+++ app/src/app/components/views/patients/table/toolbar/toolbar.component.ts
@@ -1,9 +1,14 @@
 import { Component, inject, Input } from '@angular/core';
 import { TuiButton } from '@taiga-ui/core';
+import { PolymorpheusComponent } from '@taiga-ui/polymorpheus';
+import { TuiDialogService } from '@taiga-ui/core';
 
-import PatientsTableComponent from '../table.component';
+import Sftoomi from '../../../../../class/Sftoomi';
 
 import PopupMsgService from '../../../../../services/popup-msg.service';
+import PatientEditDialogComponent, { PatientEditDialogData } from '../../dialog/dialog.component';
+
+import PatientsTableComponent from '../table.component';
 
 @Component({
     selector: 'patients-table-toolbar',
@@ -19,6 +24,7 @@ export default class PatientsTableToolbarComponent
     @Input({required: true}) table!: PatientsTableComponent;
 
     private readonly popupMsg: PopupMsgService = inject(PopupMsgService);
+    private readonly dialog: TuiDialogService = inject(TuiDialogService);
 
     protected onRefreshClick(): void
     {
@@ -41,5 +47,18 @@ export default class PatientsTableToolbarComponent
 
             return;
         }
+
+        let me: this = this;
+        this.dialog.open(new PolymorpheusComponent(PatientEditDialogComponent), {
+            label: Sftoomi.format('Edit patient #{0}', [selectedRecords[0].id]),
+            dismissible: false,
+            data: {
+                id: selectedRecords[0].id
+            } as PatientEditDialogData
+        }).subscribe({
+            complete: (): void => {
+                me.table.refresh();
+            }
+        });
     }
 }
