a9dd13207ed1a58b61cc02622865ab1fbd3aa5e6
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-17 01:01:06 +0300

    AbstractDM added

--- src/DataMappers/PatientDM.php
+++ src/DataMappers/PatientDM.php
@@ -3,35 +3,15 @@
 namespace App\DataMappers;
 
 use App\Entity\Patient;
-use App\Interfaces\DataMapper;
 
-final class PatientDM implements DataMapper
+final class PatientDM extends AbstractDM
 {
-    private array $entityColumn = [
+    protected array $entityColumns = [
         "last_name",
         "first_name",
         "middle_name",
         "dob"
     ];
-
-    /**
-     * @param array   $data
-     * @param Patient $entity
-     *
-     * @return Patient
-     */
-    public function dataToEntity(array $data, $entity): Patient
-    {
-        if (empty($data)) {
-            return $entity;
-        }
-
-        foreach ($this->entityColumn as $column) {
-            $entity = $this->addEntityValue($entity, $data, $column);
-        }
-
-        return $entity;
-    }
 
     /**
      * @param Patient $entity
@@ -60,53 +40,28 @@ final class PatientDM implements DataMapper
     }
 
     /**
-     * @param Patient $patient
-     * @param array   $data
+     * @param Patient $entity
      * @param string  $column
-     *
+     * @param mixed   $value
      * @return Patient
      */
-    protected function addEntityValue($patient, array $data, string $column): Patient
-    {
-        if ($this->columnHasValue($data, $column)) {
-            $this->applyValueByColumn($patient, $column, $data[$column]);
-
-            return $patient;
-        }
-
-        $this->applyValueByColumn($patient, $column, null);
-
-        return $patient;
-    }
-
-    /**
-     * @param array  $data
-     * @param string $column
-     *
-     * @return bool
-     */
-    private function columnHasValue(array $data, string $column): bool
-    {
-        return isset($data[$column]) && $data[$column] !== "null";
-    }
-
-    private function applyValueByColumn(Patient $patient, string $column, mixed $value): Patient
+    protected function applyValueByColumn(mixed $entity, string $column, mixed $value): Patient
     {
         switch ($column) {
             case "last_name":
-                $patient->setLastName($value);
+                $entity->setLastName($value);
                 break;
             case "first_name":
-                $patient->setFirstName($value);
+                $entity->setFirstName($value);
                 break;
             case "middle_name":
-                $patient->setMiddleName($value);
+                $entity->setMiddleName($value);
                 break;
             case "dob":
-                $patient->setDob($value);
+                $entity->setDob($value);
                 break;
         }
 
-        return $patient;
+        return $entity;
     }
 }
