88350447ddf6a67f862b4f05543d641154371d11
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-17 20:23:23 +0300

    environments added

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// Copy this file to environment.development.ts for development purpose and set it up as you want
+
+export const environment = {
+    baseUrl: 'https://specify-production-url-here' // NOTE: do not put slash symbol at the end of line
+};
