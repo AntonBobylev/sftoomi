821d673946ea329d574bbf81efafbbf29b2616fb
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-18 23:06:44 +0300

    translations

--- app/src/app/components/core/app-language-switcher/app-language-switcher.component.ts
+++ app/src/app/components/core/app-language-switcher/app-language-switcher.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TuiButton } from '@taiga-ui/core';
+import { TuiButton, TuiHintDirective } from '@taiga-ui/core';
 
 import Sftoomi from '../../../class/Sftoomi';
 
 @Component({
     selector: 'app-language-switcher',
-    imports: [TuiButton],
+    imports: [TuiButton, TuiHintDirective],
     templateUrl: './app-language-switcher.component.html',
     styleUrl: './app-language-switcher.component.scss'
 })
@@ -17,4 +17,6 @@ export default class AppLanguageSwitcherComponent
         event.stopPropagation();
         Sftoomi.Translator.switchToTheNextLocale();
     }
+
+    protected readonly Sftoomi = Sftoomi;
 }
