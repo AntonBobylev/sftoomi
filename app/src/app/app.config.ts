5ded3908c7145efa9369df4c40566b7ee95e2c3f
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-18 21:26:01 +0300

    taiga library upgrade to the latest version

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
-import { tuiValidationErrorsProvider } from '@taiga-ui/kit';
 import { TUI_LANGUAGE } from '@taiga-ui/i18n';
-import { TUI_DEFAULT_ERROR_MESSAGE, tuiDateFormatProvider } from '@taiga-ui/core';
 import { of } from 'rxjs';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { tuiDateFormatProvider } from '@taiga-ui/core';
+import { tuiValidationErrorsProvider } from '@taiga-ui/kit';
 
 import { routes } from './app.routes';
 
@@ -22,14 +22,10 @@ export const appConfig: ApplicationConfig = {
         tuiValidationErrorsProvider({
             required: Sftoomi.Translator.translate('validators.field_required'),
             maxlength: ({requiredLength}: {requiredLength: string}): string => Sftoomi.format(Sftoomi.Translator.translate('validators.max_length'), [requiredLength]),
-            only_letters: (): string => Sftoomi.Translator.translate('validators.only_letters_allowed'),
-            unfinished: (): string => Sftoomi.Translator.translate('validators.finish_filling_the_field')
+            only_letters: (): string => Sftoomi.Translator.translate('validators.only_letters_allowed')
         }), {
             provide: TUI_LANGUAGE,
-            useValue: of(Sftoomi.Translator.getI18nLocale()),
-        }, {
-            provide: TUI_DEFAULT_ERROR_MESSAGE,
-            useValue: of(Sftoomi.Translator.translate('validators.finish_filling_field'))
+            useValue: of(Sftoomi.Translator.getI18nLocale())
         },
         tuiDateFormatProvider(getDateFormat())
     ]
