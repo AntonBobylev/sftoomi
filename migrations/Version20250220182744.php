8548971d718f54aade3045dc191d4a3c92128ae2
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-20 21:34:58 +0300

    patient phone column length increased

--- migrations/Version20250220182744.php
+++ migrations/Version20250220182744.php
@@ -16,13 +16,11 @@ final class Version20250220182744 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE patient ADD phone VARCHAR(15) DEFAULT NULL');
+        $this->addSql('ALTER TABLE patient ADD phone VARCHAR(16) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE patient DROP phone');
     }
 }
