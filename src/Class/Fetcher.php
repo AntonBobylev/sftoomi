b9a1be7603a77d1f957079f99bbd29cfd114519c
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-23 16:49:37 +0300

    Fetcher class added

---
+++ src/Class/Fetcher.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Class;
+
+use DateTime;
+
+class Fetcher
+{
+    public static function int($value, $default = null): ?int
+    {
+        $result = intval($value);
+        if (!$result || self::isNull($value)) {
+            return $default;
+        }
+
+        return $result;
+    }
+
+    public static function trim($value, $default = null): ?string
+    {
+        if (!is_string($value) || self::isNull($value)) {
+            return $default;
+        }
+
+        return trim($value);
+    }
+
+    public static function date($value, $default = null): ?string
+    {
+        if (self::isNull($value)) {
+            return $default;
+        }
+
+        if (($value = strtotime($value)) !== false) {
+            $value = new DateTime()->setTimestamp($value)->format("Y-m-d");
+        } else {
+            return $default;
+        }
+
+        return $value;
+    }
+
+    public static function isNull($value): bool
+    {
+        return $value === null || $value === "null";
+    }
+}
