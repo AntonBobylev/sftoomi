628a67a0abea8ac53f43f7255c8466e3e14399d9
AntonBobylev <ya-anton3632@yandex.ru>
2025-02-23 14:28:32 +0300

    getFacilities API added

---
+++ src/Controller/FacilitiesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FacilitiesController extends AbstractController
+{
+    /**
+     * @throws Exception
+     */
+    #[Route('/getFacilities', name: 'get_facilities')]
+    public function getFacilities(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $limit = $request->request->get("limit");
+        $start = $request->request->get("start");
+        $start = $limit * $start;
+
+        $connection = $entityManager->getConnection();
+        $facilities = $connection->createQueryBuilder()
+            ->select(["id", "short_name", "full_name"])
+            ->from("facility")
+            ->setFirstResult($start)
+            ->setMaxResults($limit)
+            ->fetchAllAssociative();
+
+        $sql = "select count(*) from facility";
+        $total = $connection->fetchOne($sql);
+
+        return new JsonResponse([
+            "data"  => $facilities,
+            "total" => $total
+        ]);
+    }
+}
