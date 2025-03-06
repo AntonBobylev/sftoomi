22170ff7f0d64dd625bd15e988865739dd7f1c0a
AntonBobylev <ya-anton3632@yandex.ru>
2025-03-06 23:37:37 +0300

    remote multi select added

---
+++ app/src/app/components/fields/app-remote-multi-select/app-remote-multi-select.component.ts
@@ -0,0 +1,75 @@
+import { Component, Input, signal, WritableSignal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TuiHintDirective, TuiLoader, TuiTextfieldOptionsDirective } from '@taiga-ui/core';
+import { TuiCell } from '@taiga-ui/layout';
+import { TuiMultiSelectModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+
+import Fetcher from '../../../class/Fetcher';
+
+export type AppRemoteMultiSelectOption = {
+    id: number,
+    name: string,
+    tooltip?: string
+};
+
+export type AppRemoteMultiSelectLookupApiResult = {
+    data: AppRemoteMultiSelectOption[]
+};
+
+@Component({
+    selector: 'app-remote-multi-select',
+    templateUrl: 'app-remote-multi-select.component.html',
+    imports: [
+        TuiMultiSelectModule, TuiTextfieldOptionsDirective,
+        TuiTextfieldControllerModule, ReactiveFormsModule,
+        TuiCell, TuiLoader, TuiHintDirective
+    ],
+    styleUrl: 'app-remote-multi-select.component.scss'
+})
+
+export default class AppRemoteMultiSelectComponent
+{
+    @Input({required: true}) public label!: string;
+
+    @Input({required: true}) public lookupUrl!: string;
+
+    protected readonly store: WritableSignal<AppRemoteMultiSelectOption[]> = signal([]);
+    protected readonly isLoading: WritableSignal<boolean> = signal(false);
+
+    protected readonly stringify = ({name}: AppRemoteMultiSelectOption): string => name;
+
+    private queryController: AbortController = new AbortController();
+
+    protected onSearch(query: string | null): void
+    {
+        this.queryController.abort();
+
+        if (!query || query.length < 3) {
+            return;
+        }
+
+        this.queryController = new AbortController();
+
+        this.store.set([]);
+
+        let me: this = this,
+            data: FormData = new FormData();
+
+        data.append('query', query);
+
+        me.isLoading.set(true);
+        new Fetcher().request({
+            url: this.lookupUrl,
+            signal: this.queryController.signal,
+            data: data,
+            success: function (_response: any, _request: XMLHttpRequest, data: AppRemoteMultiSelectLookupApiResult): void {
+                me.isLoading.set(false);
+
+                me.store.set(data.data);
+            },
+            failure: function (): void {
+                me.isLoading.set(false);
+            }
+        });
+    }
+}
