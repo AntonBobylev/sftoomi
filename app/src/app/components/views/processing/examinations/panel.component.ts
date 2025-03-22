60d7e57672a25da9b3405194ceeef412cc494dde
AntonBobylev <ya-anton3632@yandex.ru>
2025-03-22 12:03:30 +0300

    examinations panel added

---
+++ app/src/app/components/views/processing/examinations/panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'processing-module-examinations-panel',
+    templateUrl: './panel.component.html',
+    styleUrl: './panel.component.less'
+})
+
+export default class ProcessingModuleExaminationsPanelComponent
+{
+
+}
--- app/src/app/components/views/processing/processing.component.ts
+++ app/src/app/components/views/processing/processing.component.ts
@@ -2,11 +2,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiAccordion } from '@taiga-ui/experimental';
 
 import Sftoomi from '../../../class/Sftoomi';
+
 import ProcessingFiltersPanelComponent, { ProcessingFiltersPanelOut } from './filters/filters.component';
+import ProcessingModuleExaminationsPanelComponent from './examinations/panel.component';
 
 @Component({
     selector: 'processing-module',
-    imports: [TuiAccordion, ProcessingFiltersPanelComponent],
+    imports: [
+        TuiAccordion, ProcessingFiltersPanelComponent,
+        ProcessingModuleExaminationsPanelComponent
+    ],
     templateUrl: './processing.component.html',
     styleUrl: './processing.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
