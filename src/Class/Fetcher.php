4a76d0b5ebbebbcd2398a81c1b726438806d53e6
AntonBobylev <ya-anton3632@yandex.ru>
2025-03-26 00:58:20 +0300

    fetcher methods fixes

--- src/Class/Fetcher.php
+++ src/Class/Fetcher.php
@@ -6,51 +6,80 @@ use DateTime;
 
 class Fetcher
 {
+    public const string SQL_DEFAULT_DATE_FORMAT = "Y-m-d";
+
     public static function int($value, $default = null): ?int
     {
-        $result = intval($value);
-        if (!$result || self::isNull($value)) {
+        if (self::isNull($value) || !is_numeric($value)) {
             return $default;
         }
 
-        return $result;
+        return intval($value);
     }
 
     public static function trim($value, $default = null): ?string
     {
-        if (!is_string($value) || self::isNull($value)) {
+        if ((!is_numeric($value) && !is_string($value)) || self::isNull($value)) {
             return $default;
         }
 
         return trim($value);
     }
 
-    public static function intArray($value, $default = null): array
+    public static function intArray($value, ?array $default = null, string $separator = ","): ?array
     {
         if (self::isNull($value)) {
             return $default;
         }
 
         if (is_string($value)) {
-            $value =  explode(',', $value);
+            if (!str_contains($value, $separator)) {
+                return $default;
+            }
+
+            $value = explode($separator, $value);
         }
 
-        return array_map('intval', $value);
+        if (!is_array($value)) {
+            return $default;
+        }
+
+        return array_map(function ($item) {
+            if (!is_numeric($item)) {
+                throw new \RuntimeException("Not numeric value detected in intArray method");
+            }
+
+            return intval($item);
+        }, $value);
     }
 
+    /**
+     * Note that we expect the row with some PHP common date format
+     * TODO: add any date formats converter
+     *
+     * @param $value
+     * @param $default
+     * @return string|null
+     */
     public static function date($value, $default = null): ?string
     {
         if (self::isNull($value)) {
             return $default;
         }
 
-        if (($value = strtotime($value)) !== false) {
-            $value = new DateTime()->setTimestamp($value)->format("Y-m-d");
-        } else {
+        if ($value instanceof DateTime) {
+            return $value->format(self::SQL_DEFAULT_DATE_FORMAT);
+        }
+
+        if (!is_string($value)) {
             return $default;
         }
 
-        return $value;
+        try {
+            return new DateTime($value)->format(self::SQL_DEFAULT_DATE_FORMAT);
+        } catch (\Exception) {
+            return $default;
+        }
     }
 
     public static function isNull($value): bool
