864c535db04ac4549e55b2c1895d598f72c7cbe5
AntonBobylev <ya-anton3632@yandex.ru>
2025-04-26 00:09:04 +0300

    patient fields added to examination dialog

--- app/src/app/components/views/processing/examinations/dialog/dialog.component.ts
+++ app/src/app/components/views/processing/examinations/dialog/dialog.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { TuiButton, TuiDialogContext, TuiLoader } from '@taiga-ui/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TuiButton, TuiDialogContext, TuiError, TuiLabel, TuiLoader, TuiTextfieldComponent } from '@taiga-ui/core';
 import { injectContext } from '@taiga-ui/polymorpheus';
+import { TuiInputDateModule } from '@taiga-ui/legacy';
+import { TuiFieldErrorPipe, TuiInputNumberDirective } from '@taiga-ui/kit';
+import { AsyncPipe } from '@angular/common';
 
 import AppBaseEditDialog from '../../../../core/app-base-edit-dialog';
+
+import PatientDemographicsTemplateComponent, { PatientDemographicsTemplateControls } from '../../../../templates/patient-demographics-template.component';
 
 export type ExaminationEditDialogData = {
     id: number
@@ -13,7 +18,11 @@ export type ExaminationEditDialogData = {
     selector: 'examination-edit-dialog',
     templateUrl: './dialog.component.html',
     imports: [
-        ReactiveFormsModule, TuiButton, TuiLoader
+        ReactiveFormsModule, TuiButton,
+        TuiLoader, TuiInputDateModule,
+        PatientDemographicsTemplateComponent,
+        TuiTextfieldComponent, AsyncPipe, TuiError,
+        TuiFieldErrorPipe, TuiLabel, TuiInputNumberDirective
     ],
     styleUrls: [
         './dialog.component.scss',
@@ -30,11 +39,27 @@ export default class ExaminationEditDialogComponent extends AppBaseEditDialog
     protected readonly saveUrl: string = '//TODO:implementMe';
 
     protected readonly form: FormGroup = new FormGroup({
-        // TODO: implement
+        patient_id:          new FormControl('', [Validators.min(1)]),
+        patient_last_name:   new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+        patient_first_name:  new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(255)]),
+        patient_middle_name: new FormControl('', [Validators.minLength(2), Validators.maxLength(255)]),
+        patient_dob:         new FormControl,
+        patient_phone:       new FormControl('', [Validators.minLength(16), Validators.maxLength(16)])
     });
 
     protected afterLoad(data: any): void
     {
         // TODO: implement
+    }
+
+    protected getPatientTemplateControls(): PatientDemographicsTemplateControls
+    {
+        return {
+            last_name:   this.form.get('patient_last_name') as FormControl,
+            first_name:  this.form.get('patient_first_name') as FormControl,
+            middle_name: this.form.get('patient_middle_name') as FormControl,
+            dob:         this.form.get('patient_dob') as FormControl,
+            phone:       this.form.get('patient_phone') as FormControl
+        }
     }
 }
