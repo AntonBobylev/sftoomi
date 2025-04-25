a866882a37163ab0aa9cec4233cb28a4e81856fe
AntonBobylev <ya-anton3632@yandex.ru>
2025-04-26 01:30:02 +0300

    lookup patient API added

--- src/Controller/PatientsController.php
+++ src/Controller/PatientsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Class\Fetcher;
+use App\Class\Format;
 use App\Repository\PatientRepository;
 use Doctrine\DBAL\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -76,5 +77,57 @@ final class PatientsController extends AppCrudController
         $this->remove($patientRepository, $request);
 
         return new JsonResponse([]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route("/lookupPatient", name: "lookup_patient")]
+    public function lookupPatient(Request $request): Response
+    {
+        $query = $request->request->get("query");
+        if (empty($query)) {
+            return new JsonResponse([]);
+        }
+
+        $excludeIds = $request->request->get("exclude_ids");
+
+        return new JsonResponse([
+            "data" => $this->tryLookupPatient($query, $excludeIds)
+        ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function tryLookupPatient(string $query, ?string $excludeIds): array
+    {
+        $sql = "select id, last_name, first_name, middle_name
+                from patient
+                where (
+                    id like '%$query%' or
+                    last_name like '%$query%' or
+                    first_name like '%$query%' or
+                    middle_name like '%$query%'
+                )";
+
+        if (!empty($excludeIds)) {
+            $sql .= " and id not in ($excludeIds)";
+        }
+
+        $patients = $this->connection->fetchAllAssociative($sql);
+        foreach ($patients as &$patient) {
+            $patient["tooltip"] = Format::humanShortName($patient);
+            $patient["name"] = $patient["id"];
+
+            unset(
+                $patient["last_name"],
+                $patient["first_name"],
+                $patient["middle_name"]
+            );
+        }
+        unset($patient);
+
+        return $patients;
     }
 }
