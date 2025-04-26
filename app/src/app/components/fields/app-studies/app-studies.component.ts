4a43b455723a67206dfcbbb0bb8778d70b8da4d9
AntonBobylev <ya-anton3632@yandex.ru>
2025-04-26 21:12:28 +0300

    app studies field added

---
+++ app/src/app/components/fields/app-studies/app-studies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-studies',
+    templateUrl: './app-studies.component.html',
+    styleUrl: './app-studies.component.scss'
+})
+
+export default class AppStudiesComponent
+{
+
+}
