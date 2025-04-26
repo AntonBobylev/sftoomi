dce4f07bcc540387017bd0089015bbcaf8e06057
AntonBobylev <ya-anton3632@yandex.ru>
2025-04-26 16:17:27 +0300

    app-combo-box added

---
+++ app/src/app/components/fields/app-combo-box/app-combo-box.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnInit, signal, WritableSignal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TuiComboBoxModule } from '@taiga-ui/legacy';
+import { TuiDataListWrapper, TuiDataListWrapperComponent, TuiFieldErrorPipe, TuiFilterByInputPipe, TuiStringifyContentPipe } from '@taiga-ui/kit';
+import { TuiError, TuiTextfieldOptionsDirective } from '@taiga-ui/core';
+
+import Sftoomi from '../../../class/Sftoomi';
+
+export type AppComboboxRecord = {
+    value: string | number,
+    title: string
+};
+
+@Component({
+    selector: 'app-combo-box',
+    templateUrl: './app-combo-box.component.html',
+    imports: [
+        AsyncPipe, ReactiveFormsModule,
+        TuiComboBoxModule, TuiDataListWrapper,
+        TuiDataListWrapperComponent, TuiError,
+        TuiFieldErrorPipe, TuiFilterByInputPipe,
+        TuiStringifyContentPipe, TuiTextfieldOptionsDirective
+    ],
+    styleUrl: './app-combo-box.component.scss'
+})
+
+export default class AppComboBoxComponent implements OnInit
+{
+    @Input({required: true}) public label!: string;
+    @Input({required: true}) public name!: string;
+    @Input({required: true, alias: 'parentFormGroup'}) public form!: FormGroup;
+    @Input({required: true}) public store: WritableSignal<AppComboboxRecord[]> = signal([]);
+
+    protected readonly Sftoomi = Sftoomi;
+
+    protected readonly stringify = (value: number): string =>
+        this.store().find(function (item: AppComboboxRecord): boolean {
+            return item.value.toString() === value.toString();
+        })?.title || '';
+
+    protected items: string[] = [];
+
+    ngOnInit(): void
+    {
+        this.items = this.store().map(({value}: AppComboboxRecord): string => value.toString());
+    }
+}
