5128fe2edfb68ccb1f61792f1521a9de96258c1a
AntonBobylev <ya-anton3632@yandex.ru>
2025-04-26 23:41:45 +0300

    app studies field behavior added

--- app/src/app/components/fields/app-studies/app-studies.component.ts
+++ app/src/app/components/fields/app-studies/app-studies.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal, WritableSignal } from '@angular/core'
+import { AfterViewInit, Component, Input, signal, WritableSignal } from '@angular/core'
 import { TuiButton, TuiHintDirective } from '@taiga-ui/core';
 import { TuiElasticContainer } from '@taiga-ui/kit';
 import { FormControl, Validators } from '@angular/forms';
@@ -10,7 +10,9 @@ import AppBaseField from '../app-base-field';
 import AppComboBoxComponent, { AppComboboxRecord } from '../app-combo-box/app-combo-box.component';
 
 type StudyCollectionRow = {
-    index: number
+    index: number,
+    store: WritableSignal<AppComboboxRecord[]>,
+    control: FormControl<string | null>
 };
 
 @Component({
@@ -25,7 +27,7 @@ type StudyCollectionRow = {
     styleUrl: './app-studies.component.scss'
 })
 
-export default class AppStudiesComponent extends AppBaseField
+export default class AppStudiesComponent extends AppBaseField implements AfterViewInit
 {
     @Input() public override label: string = 'Studies';
 
@@ -40,13 +42,28 @@ export default class AppStudiesComponent extends AppBaseField
         return this.studiesCollection.length;
     }
 
+    public ngAfterViewInit(): void
+    {
+        this.updateStudiesStores();
+    }
+
     protected onAddStudyClick(): void
     {
-        let nextIndex: number = this.lastIndex + 1;
+        let nextIndex: number = this.lastIndex + 1,
+            control: FormControl<string | null> = new FormControl<string | null>(null, [Validators.required]);
+
         this.lastIndex = nextIndex;
 
-        this.form.addControl('study' + nextIndex, new FormControl<string | null>(null, [Validators.required]));
-        this.studiesCollection.push({index: nextIndex});
+        this.form.addControl('study' + nextIndex, control);
+        this.studiesCollection.push({
+            index: nextIndex,
+            store: signal(this.store()),
+            control: control
+        });
+
+        control.valueChanges.subscribe((): void => this.updateStudiesStores());
+
+        this.updateStudiesStores();
     }
 
     protected onRemoveClick(studyIndex: number): void
@@ -55,5 +72,50 @@ export default class AppStudiesComponent extends AppBaseField
         Sftoomi.removeArrayItemByIndex(this.studiesCollection, this.studiesCollection.findIndex(function (study: StudyCollectionRow): boolean {
             return study.index === studyIndex;
         }));
+    }
+
+    protected isAddStudyClickDisabled(): boolean
+    {
+        return this.getAlreadySelectedStudies().length === this.store().length;
+    }
+
+    private updateStudiesStores(): void
+    {
+        let me: this = this,
+            alreadySelectedStudies: string[] = this.getAlreadySelectedStudies();
+
+        this.studiesCollection.forEach(function (study: StudyCollectionRow): void {
+            let selectedValue: string | null = study.control.value,
+                studiesToExclude: string[] = alreadySelectedStudies.slice();
+            if (selectedValue) {
+                let currentStudyIndex: number = alreadySelectedStudies.findIndex(function (selectedStudy: string): boolean {
+                    return selectedStudy === selectedValue;
+                });
+
+                studiesToExclude.splice(currentStudyIndex, 1);
+            }
+
+            let filteredStudies: AppComboboxRecord[] = me.store().filter(function (record: AppComboboxRecord): boolean {
+                return !studiesToExclude.includes(record.value.toString());
+            });
+
+            study.store.set(filteredStudies);
+        });
+
+    }
+
+    private getAlreadySelectedStudies(): string[]
+    {
+        let result: string[] = [];
+        this.studiesCollection.forEach(function (study: StudyCollectionRow): void {
+            let selectedValue: string | null = study.control.value;
+            if (!selectedValue) {
+                return;
+            }
+
+            result.push(selectedValue);
+        });
+
+        return result;
     }
 }
