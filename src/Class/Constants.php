56a81b659fe658c822f8530cc29d9af3d678fc35
AntonBobylev <ya-anton3632@yandex.ru>
2025-05-04 19:35:37 +0300

    Fetcher class cs

--- src/Class/Constants.php
+++ src/Class/Constants.php
@@ -6,4 +6,6 @@ class Constants
 {
     public const string ROOT_DIRECTORY = __DIR__ . "/../../";
     public const string LOG_DIRECTORY = self::ROOT_DIRECTORY . "var/log/";
+
+    public const string SQL_DEFAULT_DATE_FORMAT = "Y-m-d";
 }
--- src/Class/Fetcher.php
+++ src/Class/Fetcher.php
@@ -3,12 +3,21 @@
 namespace App\Class;
 
 use DateTime;
+use Exception;
+use RuntimeException;
 
 class Fetcher
 {
-    public const string SQL_DEFAULT_DATE_FORMAT = "Y-m-d";
-
-    public static function int($value, $default = null): ?int
+    /**
+     * Tries to convert the received value to int value.
+     * Returns default value if received value cannot be converted to int.
+     *
+     * @param mixed $value
+     * @param mixed $default
+     *
+     * @return int|null
+     */
+    public static function int(mixed $value, mixed $default = null): int | null
     {
         if (self::isNull($value) || !is_numeric($value)) {
             return $default;
@@ -17,7 +26,16 @@ class Fetcher
         return intval($value);
     }
 
-    public static function trim($value, $default = null): ?string
+    /**
+     * Tries to convert the received value to string and trim it.
+     * Returns default value if received value is not numeric and not string, or received value is null.
+     *
+     * @param mixed $value
+     * @param mixed $default
+     *
+     * @return string|null
+     */
+    public static function trim(mixed $value, mixed $default = null): string | null
     {
         if ((!is_numeric($value) && !is_string($value)) || self::isNull($value)) {
             return $default;
@@ -26,7 +44,17 @@ class Fetcher
         return trim($value);
     }
 
-    public static function intArray($value, ?array $default = null, string $separator = ","): ?array
+    /**
+     * Tries to convert the received value to array of integer values.
+     * Returns default value if received value cannot be converted to integer values.
+     *
+     * @param mixed      $value
+     * @param array|null $default
+     * @param string $separator
+     *
+     * @return array|null
+     */
+    public static function intArray(mixed $value, ?array $default = null, string $separator = ","): array | null
     {
         if (self::isNull($value)) {
             return $default;
@@ -42,7 +70,7 @@ class Fetcher
 
         return array_map(function ($item) {
             if (!is_numeric($item)) {
-                throw new \RuntimeException("Not numeric value detected in intArray method");
+                throw new RuntimeException("Not numeric value detected in intArray method");
             }
 
             return intval($item);
@@ -50,21 +78,29 @@ class Fetcher
     }
 
     /**
-     * Note that we expect the row with some PHP common date format
-     * TODO: add any date formats converter
+     * Tries to convert the received value to date, then formats it to passed format and returns it as string.
+     * Returns default value if received value cannot be converted to date.
      *
-     * @param $value
-     * @param $default
+     * TODO: add any date formats converter because right now we expect the row with some PHP common date format right now
+     *
+     * @param mixed  $value
+     * @param mixed  $default
+     * @param string $format
+     *
      * @return string|null
      */
-    public static function date($value, $default = null): ?string
+    public static function date(
+        mixed $value,
+        mixed $default = null,
+        string $format = Constants::SQL_DEFAULT_DATE_FORMAT
+    ): string | null
     {
         if (self::isNull($value)) {
             return $default;
         }
 
         if ($value instanceof DateTime) {
-            return $value->format(self::SQL_DEFAULT_DATE_FORMAT);
+            return $value->format($format);
         }
 
         if (!is_string($value)) {
@@ -72,12 +108,19 @@ class Fetcher
         }
 
         try {
-            return new DateTime($value)->format(self::SQL_DEFAULT_DATE_FORMAT);
-        } catch (\Exception) {
+            return new DateTime($value)->format($format);
+        } catch (Exception) {
             return $default;
         }
     }
 
+    /**
+     * Checks the value is null or not
+     *
+     * @param $value
+     *
+     * @return bool
+     */
     public static function isNull($value): bool
     {
         return $value === null || $value === "null";
