f3410662bf74ddf67ec82df22e4fb2bac6fdee1a
AntonBobylev <ya-anton3632@yandex.ru>
2025-05-04 19:40:00 +0300

    Format class cs

--- src/Class/Format.php
+++ src/Class/Format.php
@@ -4,20 +4,29 @@ namespace App\Class;
 
 class Format
 {
-    public static function humanShortName($arg1, $arg2 = null, $arg3 = null): string
+    /**
+     * Converts the received arguments and formats to string.
+     * @example $arg1: "Doe", $arg2: "John", $arg3: "Smith", the result: "Doe J.S."
+     *
+     * @param mixed $arg1
+     * @param mixed $arg2
+     * @param mixed $arg3
+     *
+     * @return string
+     */
+    public static function humanShortName(mixed $arg1, mixed $arg2 = null, mixed $arg3 = null): string
     {
         if (is_array($arg1)) {
-            $last = $arg1["last_name"];
-            $first = $arg1["first_name"];
+            $last   = $arg1["last_name"];
+            $first  = $arg1["first_name"];
             $middle = $arg1["middle_name"];
         } else {
-            $last = $arg1;
-            $first = $arg2;
+            $last   = $arg1;
+            $first  = $arg2;
             $middle = $arg3;
         }
 
         $last = trim($last);
-
         $first = substr(trim($first), 0, 1);
         if ($first != "") {
             $first .= ".";
