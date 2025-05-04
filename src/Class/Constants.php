35ed122a6d216478ad6acbacd713151a83e4ab73
AntonBobylev <ya-anton3632@yandex.ru>
2025-05-04 19:19:27 +0300

    Constants class added

---
+++ src/Class/Constants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Class;
+
+class Constants
+{
+    public const string ROOT_DIRECTORY = __DIR__ . "/../../";
+    public const string LOG_DIRECTORY = self::ROOT_DIRECTORY . "var/log/";
+}
