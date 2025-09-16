33f1ba1b86c4d7759de94fe4020b0490023e52bc
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-16 23:33:38 +0300

    code-editor pack added to template component

--- app/src/app/components/views/template/template.component.ts
+++ app/src/app/components/views/template/template.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { CodeEditorComponent, CodeModel } from '@ngstack/code-editor';
+
+import Sftoomi from '../../../class/Sftoomi';
+
+import { SafePipe } from '../../../pipes/safe.pipe';
 
 @Component({
     selector: 'template-module',
     templateUrl: './template.component.html',
+    imports: [
+        CodeEditorComponent,
+        SafePipe,
+    ],
     styleUrl: './template.component.less'
 })
 
 export default class TemplateComponent
 {
+    protected readonly templateCode: WritableSignal<string> = signal<string>(this.getDefaultValue());
+    protected readonly Sftoomi = Sftoomi;
+
+    model: CodeModel = {
+        language: 'html',
+        uri: 'main.html',
+        value: this.getDefaultValue()
+    };
+
+    options = {
+        contextmenu: false,
+        lineNumbers: true,
+        minimap: {
+            enabled: true,
+        }
+    };
+
+    onCodeChanged(value: any): void
+    {
+        this.templateCode.set(value);
+    }
+
+    getDefaultValue(): string
+    {
+        return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .test {
+            color: red;
+        }
+    </style>
+</head>
+    <body>
+        <div class="test">test</div>
+    </body>
+</html>`;
+    }
 }
