c43da3cc1a86b1f25a1958ce811dafb87041fe06
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-18 20:58:10 +0300

    tempalte edit dialog added

--- app/src/app/components/views/templates/table/toolbar/toolbar.component.ts
+++ app/src/app/components/views/templates/table/toolbar/toolbar.component.ts
@@ -1,9 +1,13 @@
 import { Component} from '@angular/core';
 import { TuiButton, TuiHint } from '@taiga-ui/core';
+import { PolymorpheusComponent } from '@taiga-ui/polymorpheus';
+import { defaultIfEmpty } from 'rxjs';
 
 import Sftoomi from '../../../../../class/Sftoomi';
 
 import AppBaseToolbar from '../../../../core/app-base-toolbar';
+
+import TemplateEditDialogComponent, { TemplateEditDialogData } from '../../dialog/dialog.component';
 
 @Component({
     selector: 'templates-module-table-toolbar',
@@ -21,20 +25,18 @@ export default class TemplatesModuleTableToolbarComponent extends AppBaseToolbar
 
     protected openEditDialog(title: string, id?: number): void
     {
-        // TODO: implement
-
-        // let me: this = this;
-        // this.dialog.open(new PolymorpheusComponent(FacilityEditDialogComponent  ), {
-        //     label: title,
-        //     data: {
-        //         id: id
-        //     } as FacilityEditDialogData
-        // })
-        //     .pipe(defaultIfEmpty({saved: false}))
-        //     .subscribe((result: any): void => {
-        //         if (result?.saved) {
-        //             me.table.refresh();
-        //         }
-        //     });
+        let me: this = this;
+        this.dialog.open(new PolymorpheusComponent(TemplateEditDialogComponent), {
+            label: title,
+            data: {
+                id: id
+            } as TemplateEditDialogData
+        })
+            .pipe(defaultIfEmpty({saved: false}))
+            .subscribe((result: any): void => {
+                if (result?.saved) {
+                    me.table.refresh();
+                }
+            });
     }
 }
