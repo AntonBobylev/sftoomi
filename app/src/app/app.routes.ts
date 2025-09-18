4d41c8c57ccc1bf85fe4fd9f69c9b944db0088bc
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-18 22:26:31 +0300

    patients module added to navigation

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import Sftoomi from './class/Sftoomi';
 import HomeComponent from './views/home/home.component';
+import PatientsComponent from './views/patients/patients.component';
 
 const BASE_TITLE = 'SFTOOMI :: ';
 
@@ -20,20 +21,16 @@ export const routes: Routes = [{
     path: RoutesPaths.HOME,
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.home'),
     component: HomeComponent
+}, {
+    path: RoutesPaths.PATIENTS,
+    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.patients'),
+    component: PatientsComponent
 }];
 
 // export const routes: Routes = [{
-//     path: RoutesPaths.HOME,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.home'),
-//     component: HomeComponent
-// }, {
 //     path: RoutesPaths.PROCESSING,
 //     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.processing'),
 //     component: ProcessingComponent
-// }, {
-//     path: RoutesPaths.PATIENTS,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.patients'),
-//     component: PatientsComponent
 // }, {
 //     path: RoutesPaths.FACILITIES,
 //     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.facilities'),
--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
+import Sftoomi from './class/Sftoomi';
+
+import { RoutesPaths } from './app.routes';
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, NzLayoutModule, NzMenuModule],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, NzLayoutModule, NzMenuModule, RouterLink, RouterLinkActive],
+    templateUrl: './app.html',
+    styleUrl: './app.scss'
 })
-export class App {}
+
+export class App
+{
+    protected readonly Sftoomi = Sftoomi;
+    protected readonly RoutesPaths = RoutesPaths;
+}
