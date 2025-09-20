11ee51ac5c932cc3bd4fe925e0a3af037049f685
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-20 16:08:42 +0300

    patient demographics template added

---
+++ app/src/app/components/templates/templates/patient-demographics-template/patient-demographics-template.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NzInputDirective } from 'ng-zorro-antd/input';
+import { NzColDirective } from 'ng-zorro-antd/grid';
+import { NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent } from 'ng-zorro-antd/form';
+
+import Sftoomi from '../../../../class/Sftoomi';
+
+import FormErrorComponent from '../form-error/form-error.component';
+
+@Component({
+    selector: 'patient-demographics-template',
+    templateUrl: './patient-demographics-template.component.html',
+    imports: [
+        ReactiveFormsModule,
+        NzInputDirective,
+        NzFormDirective,
+        NzFormItemComponent,
+        NzFormLabelComponent,
+        NzFormControlComponent,
+        NzColDirective,
+        FormErrorComponent
+    ],
+    styleUrl: './patient-demographics-template.component.less'
+})
+export default class PatientDemographicsTemplateComponent
+{
+    @Input({required: true}) public form!: FormGroup;
+
+    protected readonly Sftoomi = Sftoomi;
+}
