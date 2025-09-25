0c0776f3b5aa97301b381660cb1d29fd5f3d9120
AntonBobylev <ya-anton3632@yandex.ru>
2025-09-25 15:17:10 +0300

    phone field added to patient demographics template

--- app/src/app/components/templates/patient-demographics-template/patient-demographics-template.component.ts
+++ app/src/app/components/templates/patient-demographics-template/patient-demographics-template.component.ts
@@ -1,6 +1,7 @@
 import { NgTemplateOutlet } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MaskitoDirective } from '@maskito/angular';
 import { NzInputDirective, NzInputGroupComponent } from 'ng-zorro-antd/input';
 import { NzColDirective } from 'ng-zorro-antd/grid';
 import { NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent } from 'ng-zorro-antd/form';
@@ -8,6 +9,8 @@ import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
 
 import Sftoomi from '../../../class/Sftoomi';
+
+import phoneMask from '../../../misc/phone-mask-generator';
 
 import FormErrorTemplateComponent from '../form-error-template/form-error-template.component';
 
@@ -19,19 +22,11 @@ import getDatePickerLocalDateFormat from '../../../locale/getDatePickerLocalDate
     selector: 'patient-demographics-template',
     templateUrl: './patient-demographics-template.component.html',
     imports: [
-        ReactiveFormsModule,
-        NzInputDirective,
-        NzFormDirective,
-        NzFormItemComponent,
-        NzFormLabelComponent,
-        NzFormControlComponent,
-        NzColDirective,
-        FormErrorTemplateComponent,
-        NzDatePickerComponent,
-        NzInputGroupComponent,
-        NzIconDirective,
-        NgTemplateOutlet,
-        UppercaseDirective
+        ReactiveFormsModule, NzInputDirective, NzFormDirective,
+        NzFormItemComponent, NzFormLabelComponent, NzFormControlComponent,
+        NzColDirective, FormErrorTemplateComponent, NzDatePickerComponent,
+        NzInputGroupComponent, NzIconDirective, NgTemplateOutlet,
+        UppercaseDirective, MaskitoDirective
     ],
     styleUrl: './patient-demographics-template.component.less'
 })
@@ -42,4 +37,6 @@ export default class PatientDemographicsTemplateComponent
     protected readonly Sftoomi = Sftoomi;
 
     protected readonly getDatePickerLocalDateFormat = getDatePickerLocalDateFormat;
+
+    protected readonly phoneMask = phoneMask;
 }
