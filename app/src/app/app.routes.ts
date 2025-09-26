fa5b234b1717a27a520c3c8911e04a5293facdc4
AntonBobylev <ya-anton3632@yandex.ru>
2025-09-27 00:04:43 +0300

    garbage removed

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -8,13 +8,7 @@ const BASE_TITLE = 'SFTOOMI :: ';
 
 export enum RoutesPaths {
     HOME = '',
-    PROCESSING = 'processing',
-    PATIENTS = 'patients',
-    FACILITIES = 'facilities',
-    DOCTORS = 'doctors',
-    STUDIES = 'studies',
-    TEMPLATE = 'template',
-    TEMPLATES = 'templates',
+    PATIENTS = 'patients'
 }
 
 export const routes: Routes = [{
@@ -26,29 +20,3 @@ export const routes: Routes = [{
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.patients'),
     component: PatientsComponent
 }];
-
-// export const routes: Routes = [{
-//     path: RoutesPaths.PROCESSING,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.processing'),
-//     component: ProcessingComponent
-// }, {
-//     path: RoutesPaths.FACILITIES,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.facilities'),
-//     component: FacilitiesComponent
-// }, {
-//     path: RoutesPaths.DOCTORS,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.doctors'),
-//     component: DoctorsComponent
-// }, {
-//     path: RoutesPaths.STUDIES,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.studies'),
-//     component: StudiesComponent
-// }, {
-//     path: RoutesPaths.TEMPLATE,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.template'),
-//     component: TemplateComponent
-// }, {
-//     path: RoutesPaths.TEMPLATES,
-//     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.templates'),
-//     component: TemplatesModuleComponent
-// }];
