f964fa49ffa7d182174eef5aaabd9e775ed23a5a
AntonBobylev <ya-anton3632@yandex.ru>
2025-09-26 23:32:58 +0300

    app datepicker field applied

--- app/src/app/components/templates/patient-demographics-template/patient-demographics-template.component.ts
+++ app/src/app/components/templates/patient-demographics-template/patient-demographics-template.component.ts
@@ -5,7 +5,6 @@ import { MaskitoDirective } from '@maskito/angular';
 import { NzInputDirective, NzInputGroupComponent } from 'ng-zorro-antd/input';
 import { NzColDirective } from 'ng-zorro-antd/grid';
 import { NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent } from 'ng-zorro-antd/form';
-import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
 
 import Sftoomi from '../../../class/Sftoomi';
@@ -15,7 +14,7 @@ import phoneMask from '../../../misc/phone-mask-generator';
 import FormErrorTemplateComponent from '../form-error-template/form-error-template.component';
 import AppTextfieldComponent from '../../core/app-textfield/app-textfield.component';
 
-import getDatePickerLocalDateFormat from '../../../locale/getDatePickerLocalDateFormat';
+import AppDatepickerComponent from '../../core/app-datepicker/app-datepicker.component';
 
 @Component({
     selector: 'patient-demographics-template',
@@ -23,9 +22,9 @@ import getDatePickerLocalDateFormat from '../../../locale/getDatePickerLocalDate
     imports: [
         ReactiveFormsModule, NzInputDirective, NzFormDirective,
         NzFormItemComponent, NzFormLabelComponent, NzFormControlComponent,
-        NzColDirective, FormErrorTemplateComponent, NzDatePickerComponent,
+        NzColDirective, FormErrorTemplateComponent,
         NzInputGroupComponent, NzIconDirective, NgTemplateOutlet,
-        MaskitoDirective, AppTextfieldComponent
+        MaskitoDirective, AppTextfieldComponent, AppDatepickerComponent
     ],
     styleUrl: './patient-demographics-template.component.less'
 })
@@ -34,8 +33,6 @@ export default class PatientDemographicsTemplateComponent
     @Input({required: true}) public form!: FormGroup;
 
     protected readonly Sftoomi = Sftoomi;
-
-    protected readonly getDatePickerLocalDateFormat = getDatePickerLocalDateFormat;
 
     protected readonly phoneMask = phoneMask;
 }
