a8bc53487939cc294b4dfb3ed839b737370c7b15
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-28 23:32:23 +0300

    referring doctors module added

---
+++ app/src/app/views/referring-doctors/referring-doctors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-referring-doctors',
+    imports: [],
+    templateUrl: './referring-doctors.component.html',
+    styleUrl: './referring-doctors.component.scss'
+})
+export default class ReferringDoctorsComponent
+{
+}
