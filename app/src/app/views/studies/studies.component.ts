25bb1c3c45e38ce9abacd3301eabe00726a13db1
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-29 21:09:30 +0300

    studies table added

--- app/src/app/views/studies/studies.component.ts
+++ app/src/app/views/studies/studies.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import StudiesTableComponent from './table/table.component';
+
 
 @Component({
     selector: 'app-studies',
     templateUrl: './studies.component.html',
+    imports: [
+        StudiesTableComponent
+    ],
     styleUrl: './studies.component.scss'
 })
 export default class StudiesComponent
---
+++ app/src/app/views/studies/table/table.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+
+import AppTableImports from '../../../components/core/app-table/imports';
+
+import Sftoomi from '../../../class/Sftoomi';
+
+import AppTableComponent from '../../../components/core/app-table/app-table.component';
+
+import getStudiesAPI from '../../../APIs/getStudiesAPI';
+
+import AppTableColumn from '../../../type/AppTableColumn';
+
+@Component({
+    selector: 'studies-table',
+    templateUrl: '../../../components/core/app-table/app-table.component.html',
+    styleUrl: '../../../components/core/app-table/app-table.component.scss',
+    imports: [ AppTableImports ]
+})
+
+export default class StudiesTableComponent extends AppTableComponent
+{
+    protected override readonly columns: AppTableColumn[] = [{
+        name: 'id',
+        width: '60px',
+        header: {
+            caption: Sftoomi.Translator.translate('id'),
+            extraStyles: {
+                justifyContent: 'center'
+            }
+        },
+        dataCell: {
+            extraStyles: {
+                justifyContent: 'center'
+            }
+        }
+    }, {
+        name: 'short_name',
+        width: '200px',
+        header: {
+            caption: Sftoomi.Translator.translate('short_name')
+        }
+    }, {
+        name: 'full_name',
+        width: '400px',
+        header: {
+            caption: Sftoomi.Translator.translate('full_name')
+        }
+    }, {
+        name: 'study_cpts',
+        header: {
+            caption: Sftoomi.Translator.translate('full_name'),
+            tooltipRenderer: (): string => 'CPT codes assigned to the current study'
+        },
+        rawHtml: true,
+        valueRenderer: (cpts: getStudiesAPI['data'][0]['study_cpts']): string => {
+            if (Sftoomi.isEmpty(cpts)) {
+                return Sftoomi.Translator.translate('not_set');
+            }
+
+            let lines: string[] = [];
+            cpts.forEach(cpt => {
+                lines.push(
+                    '<ul style="margin: unset">' +
+                        '<li>' + cpt.code + ' (' + cpt.short_name + ')' + '</li>' +
+                    '</ul>'
+                );
+            });
+
+            return `<div>${lines.join('')}</div>`;
+        }
+    }];
+
+    protected override readonly loadUrl: string = '/getStudies';
+
+    // protected override readonly toolbar: any | undefined = ReferringFacilitiesTableToolbarComponent;
+}
