d23e7ab9501f9512d95d569e0660afa1ace70d5d
Anton Bobylev <ya-anton3632@yandex.ru>
2025-09-30 19:44:00 +0300

    examinations module added

---
+++ app/src/app/views/examinations/examinations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-examinations',
+    templateUrl: './examinations.component.html',
+    styleUrl: './examinations.component.scss'
+})
+
+export default class ExaminationsComponent
+{
+
+}
