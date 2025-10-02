42a69c79ee2e6d236686b201a3cd12bbc1947300
AntonBobylev <ya-anton3632@yandex.ru>
2025-10-02 18:15:53 +0300

    responsive layout service applied for home module

--- app/src/app/views/home/home.component.ts
+++ app/src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 import Sftoomi from '../../class/Sftoomi';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [NgClass],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
