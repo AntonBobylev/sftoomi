ffae8f7d22061e15311e0845965f85210043036e
AntonBobylev <ya-anton3632@yandex.ru>
2025-10-02 18:05:52 +0300

    responsive layout service added

---
+++ app/src/app/services/responsive-layout.service.ts
@@ -0,0 +1,32 @@
+import { computed, inject, Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ResponsiveLayoutService
+{
+    breakpointObserver = inject(BreakpointObserver);
+
+    screenWidth = toSignal(this.breakpointObserver.observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge
+    ]))
+
+    isSmallWidth = computed((): boolean =>
+        this.screenWidth()!.breakpoints[Breakpoints.XSmall] || this.screenWidth()!.breakpoints[Breakpoints.Small]
+    );
+
+    isMediumWidth = computed(() =>
+        this.screenWidth()!.breakpoints[Breakpoints.Medium]
+    );
+
+    isLargeWidth = computed(() =>
+        this.screenWidth()!.breakpoints[Breakpoints.Large] || this.screenWidth()!.breakpoints[Breakpoints.XLarge]
+    );
+}
