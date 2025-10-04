8afa3d7005c66f506b4c4d0e528f7d815122f463
Anton Bobylev <ya-anton3632@yandex.ru>
2025-10-04 15:38:07 +0300

    examinations filters added

--- app/src/app/views/examinations/examinations.component.ts
+++ app/src/app/views/examinations/examinations.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import ExaminationsTableComponent from './table/table.component';
+import { ExaminationsFiltersComponent } from './filters/filters.component';
 
 @Component({
     selector: 'app-examinations',
     templateUrl: './examinations.component.html',
     imports: [
-        ExaminationsTableComponent
+        ExaminationsTableComponent,
+        ExaminationsFiltersComponent
     ],
     styleUrl: './examinations.component.scss'
 })
---
+++ app/src/app/views/examinations/filters/filters.component.ts
@@ -0,0 +1,94 @@
+import { AfterViewInit, Component, EventEmitter, Output, signal, WritableSignal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import Fetcher from '../../../class/Fetcher';
+import Sftoomi from '../../../class/Sftoomi';
+import { DialogType } from '../../../class/Dialog';
+
+import AppNumberfieldComponent from '../../../components/core/app-numberfield/app-numberfield.component'
+
+import getExaminationsFiltersAPI from '../../../APIs/getExaminationsFiltersAPI';
+
+export type ProcessingFiltersPanelOut = {
+    examination_date: Date,
+    examination_id:   number | null
+}
+
+@Component({
+    selector: 'examinations-module-filters-panel',
+    templateUrl: './filters.component.html',
+    imports: [
+        ReactiveFormsModule,
+        AppNumberfieldComponent
+    ],
+    styleUrl: './filters.component.less'
+})
+
+export class ExaminationsFiltersComponent implements AfterViewInit
+{
+    @Output() public onSearch: EventEmitter<ProcessingFiltersPanelOut> = new EventEmitter<ProcessingFiltersPanelOut>();
+    @Output() public onClear: EventEmitter<ProcessingFiltersPanelOut> = new EventEmitter<ProcessingFiltersPanelOut>();
+    @Output() public onLoaded: EventEmitter<ProcessingFiltersPanelOut> = new EventEmitter<ProcessingFiltersPanelOut>();
+
+    protected readonly form: FormGroup = new FormGroup({
+        examination_id: new FormControl<number | null>(null, [Validators.min(1), Validators.max(Sftoomi.Constants.types.int.unsigned)])
+    });
+
+    protected readonly data: WritableSignal<getExaminationsFiltersAPI['data'] | undefined> = signal<getExaminationsFiltersAPI['data'] | undefined>(undefined);
+    protected readonly isLoading: WritableSignal<boolean> = signal<boolean>(false);
+
+    private readonly getExaminationsFiltersUrl: string = '/getExaminationsFilters';
+
+    ngAfterViewInit(): void
+    {
+        this.isLoading.set(true);
+
+        new Fetcher().request({
+            url: this.getExaminationsFiltersUrl,
+            success: (_response: any, _request: any, data: getExaminationsFiltersAPI): void => {
+                this.isLoading.set(false);
+
+                if (Sftoomi.isEmpty(data.data)) {
+                    return;
+                }
+
+                this.data.set(data.data);
+
+                this.onLoaded.emit(this.getValues());
+            },
+            failure: (_code: any, message: any, _request: any): void => {
+                this.isLoading.set(false);
+
+                if (message === 'canceled') {
+                    return;
+                }
+
+                Sftoomi.Dialog.show(message, DialogType.ERROR);
+            }
+        })
+    }
+
+    public getValues(): ProcessingFiltersPanelOut
+    {
+        return {
+            examination_date: new Date(),
+            examination_id:   this.form.get('examination_id')?.value ?? ''
+        };
+    }
+
+    public clearForm(): void
+    {
+        this.form.get('examination_id')?.setValue(null);
+
+        this.onClear.emit(this.getValues());
+    }
+
+    protected onExaminationDateClick(date: any): void
+    {
+         this.onSearch.emit(this.getValues());
+    }
+
+    protected onTodayClick(): void
+    {
+    }
+}
