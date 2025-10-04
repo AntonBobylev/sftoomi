1dd518fd7888e0ee87278420cd30835910931f65
Anton Bobylev <ya-anton3632@yandex.ru>
2025-10-04 15:36:34 +0300

    Constants class added

---
+++ app/src/app/class/Constants.ts
@@ -0,0 +1,10 @@
+export default class Constants
+{
+    public readonly types = {
+        int: {
+            min: -2147483648,
+            max: 2147483647,
+            unsigned: 4294967295
+        }
+    }
+}
