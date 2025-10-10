4c2ed68d93c989a636a6aa440c6303e778c732aa
AntonBobylev <ya-anton3632@yandex.ru>
2025-10-10 19:17:33 +0300

    getUsers API added

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\DBAL\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UsersController extends AppCrudController
+{
+    protected string $baseTable = "users";
+
+    /**
+     * @throws Exception
+     */
+    #[Route("/getUsers", name: "get_users")]
+    public function getUsers(Request $request): Response
+    {
+        $patients = $this->getList(
+            $request,
+            ["id", "login", "last_name", "first_name", "roles", "created_at"]
+        );
+
+        return new JsonResponse([
+            "data"  => $patients["data"],
+            "total" => $patients["total"]
+        ]);
+    }
+}
