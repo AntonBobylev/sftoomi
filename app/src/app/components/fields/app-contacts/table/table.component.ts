e799606d121e3fe6fa292a3b795868de15410026
AntonBobylev <ya-anton3632@yandex.ru>
2025-10-12 21:20:25 +0300

    contacts table columns edits

--- app/src/app/components/fields/app-contacts/table/table.component.ts
+++ app/src/app/components/fields/app-contacts/table/table.component.ts
@@ -8,6 +8,15 @@ import AppTableComponent from '../../../core/app-table/app-table.component';
 
 import AppTableColumn from '../../../../type/AppTableColumn';
 
+export type AppContactsTableRecord = {
+    item_id:  number,
+    type:     AppContactType,
+    position: number,
+    text:     string
+};
+
+export type AppContactType = 'email' | 'phone' | 'address';
+
 @Component({
     selector: 'app-contacts-table',
     imports: [ AppTableImports ],
@@ -17,10 +26,10 @@ import AppTableColumn from '../../../../type/AppTableColumn';
 export default class AppContactsTableComponent extends AppTableComponent
 {
     protected override readonly columns: AppTableColumn[] = [{
-        name: 'type',
+        name: 'position',
         width: '60px',
         header: {
-            caption: Sftoomi.Translator.translate('fields.contacts.type'),
+            caption: Sftoomi.Translator.translate('fields.contacts.position'),
             extraStyles: {
                 justifyContent: 'center'
             }
@@ -29,12 +38,25 @@ export default class AppContactsTableComponent extends AppTableComponent
             extraStyles: {
                 justifyContent: 'center'
             }
-        }
+        },
+        valueRenderer: (value: AppContactsTableRecord['position']): string => Sftoomi.numberToOrdinal(value)
     }, {
-        name: 'position',
-        width: '60px',
+        name: 'type',
+        width: '120px',
         header: {
-            caption: Sftoomi.Translator.translate('fields.contacts.position')
+            caption: Sftoomi.Translator.translate('fields.contacts.type'),
+        },
+        valueRenderer: (type: AppContactsTableRecord['type']): string => {
+            switch (type) {
+                case 'email':
+                    return Sftoomi.Translator.translate('fields.contacts.email');
+                case 'phone':
+                    return Sftoomi.Translator.translate('fields.contacts.phone');
+                case 'address':
+                    return Sftoomi.Translator.translate('fields.contacts.address');
+                default:
+                    return Sftoomi.Translator.translate('not_set_tip');
+            }
         }
     }, {
         name: 'text',
