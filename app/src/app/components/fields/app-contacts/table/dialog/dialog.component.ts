c1d0bf9bcae582a3950707760ee82434bc11bbe3
Anton Bobylev <ya-anton3632@yandex.ru>
2025-10-14 19:49:13 +0300

    contacts edit dialog filling added

--- app/src/app/components/fields/app-contacts/table/dialog/dialog.component.ts
+++ app/src/app/components/fields/app-contacts/table/dialog/dialog.component.ts
@@ -1,10 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { NzButtonComponent } from 'ng-zorro-antd/button'
 import { NZ_MODAL_DATA, NzModalFooterDirective } from 'ng-zorro-antd/modal'
 
 import AppBaseEditDialog from '../../../../core/app-base-edit-dialog/app-base-edit-dialog';
 import { AppContactType } from '../table.component'
+import AppComboComponent, { AppComboRecord } from '../../../../core/app-combo/app-combo.component'
+import AppTextfieldComponent from '../../../../core/app-textfield/app-textfield.component'
+import Sftoomi from '../../../../../class/Sftoomi'
 
 export type ContactsEditDialogData = {
     item_id?: number,
@@ -19,20 +22,48 @@ export type ContactsEditDialogData = {
     imports: [
         ReactiveFormsModule,
         NzModalFooterDirective,
-        NzButtonComponent
+        NzButtonComponent,
+        AppComboComponent,
+        AppTextfieldComponent
     ],
     styleUrl: './dialog.component.less'
 })
 
 export default class ContactsEditDialogComponent extends AppBaseEditDialog
 {
+    @ViewChild('typeCtrl')
+    protected readonly typeCtrl!: AppComboComponent;
+
     protected override readonly data: ContactsEditDialogData = inject(NZ_MODAL_DATA);
 
     protected readonly form: FormGroup = new FormGroup({
+        type: new FormControl<AppContactType | null>(null, [Validators.required]),
+        text: new FormControl<string | null>(null, [Validators.required])
     });
+
+    protected override readonly width: string | number | undefined = 300;
+
+    protected readonly availableTypes: AppComboRecord[] = [{
+        caption: Sftoomi.Translator.translate('fields.contacts.email'),
+        value: 'email'
+    }, {
+        caption: Sftoomi.Translator.translate('fields.contacts.email'),
+        value: 'address'
+    }, {
+        caption: Sftoomi.Translator.translate('fields.contacts.phone'),
+        value: 'phone'
+    }];
 
     protected afterLoad(data?: ContactsEditDialogData): void
     {
+        this.typeCtrl.setData(this.availableTypes);
+
+        if (!data) {
+            return;
+        }
+
+        this.form.get('type')?.setValue(data.type);
+        this.form.get('text')?.setValue(data.text);
     }
 
     protected override getAdditionalDataOnSave(data: FormData): FormData
