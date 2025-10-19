d6f739107cef3022dd921b6669873bc523d0a8aa
Anton Bobylev <ya-anton3632@yandex.ru>
2025-10-19 16:57:22 +0300

    saveUser API added

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Class\Contacts;
+use App\Class\Fetcher;
 use Doctrine\DBAL\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -54,5 +55,45 @@ final class UsersController extends AppCrudController
         return new JsonResponse([
             "data" => $data
         ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route("/saveUser", name: "save_user")]
+    public function saveUser(Request $request, Contacts $contacts): Response
+    {
+        try {
+            $this->connection->beginTransaction();
+            $contactId = $contacts->set(Fetcher::json($request->request->get("contacts")));
+
+            $values = [
+                "id"          => Fetcher::int($request->request->get("id")),
+                "login"       => Fetcher::trim($request->request->get("login")),
+                "reset_password"           => Fetcher::int($request->request->get("reset_password". false)),
+                "force_to_change_password" => Fetcher::int($request->request->get("force_to_change_password", false)),
+                "disabled"                 => Fetcher::int($request->request->get("disabled", false)),
+                "last_name"   => Fetcher::trim($request->request->get("last_name"), ""),
+                "first_name"  => Fetcher::trim($request->request->get("first_name"), ""),
+                "contact_id"  => $contactId
+            ];
+
+            $result = $this->save(
+                $request,
+                $values,
+                ["login"]
+            );
+        } catch (Exception $ex) {
+            $this->connection->rollBack();
+
+            throw $ex;
+        }
+
+        $this->connection->commit();
+
+        return new JsonResponse([
+            "id" => $result["id"]
+        ]);
+
     }
 }
