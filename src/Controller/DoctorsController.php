af12979d08127b2ddfbcee785a18b11ceac40737
Anton Bobylev <ya-anton3632@yandex.ru>
2025-11-04 15:23:38 +0300

    getDoctors API edits

--- src/Controller/DoctorsController.php
+++ src/Controller/DoctorsController.php
@@ -3,40 +3,28 @@
 namespace App\Controller;
 
 use App\Class\Fetcher;
+use App\Class\Model\DoctorModel;
 use Doctrine\DBAL\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class DoctorsController extends AppCrudController
+final class DoctorsController extends SftoomiController
 {
-    protected string $baseTable = "doctor";
-
-    /**
-     * @throws Exception
-     */
     #[Route("/getDoctors", name: "get_doctors")]
     public function getDoctors(Request $request): Response
     {
-        $doctors = $this->getList(
-            $request,
-            ["id", "last_name", "first_name", "middle_name"]
+        $doctorModel = new DoctorModel($this->connection);
+
+        $result = $doctorModel->getAll(
+            $request->request->get("start"),
+            $request->request->get("limit")
         );
 
-        $data = $doctors["data"];
-        foreach ($data as &$row) {
-            $sql = "select f.id, f.short_name, f.full_name
-                    from facilities_doctors fd
-                        left join facility f on f.id = fd.facility_id
-                    where fd.doctor_id = {$row["id"]}";
-            $row["doctor_facilities"] = $this->connection->fetchAllAssociative($sql);
-        }
-        unset($row);
-
         return new JsonResponse([
-            "data"  => $data,
-            "total" => $doctors["total"]
+            "data"  => $result["data"],
+            "total" => $result["total"]
         ]);
     }
 
