ca0c68c7dca227593be227770a7a740d181abc68
Anton Bobylev <ya-anton3632@yandex.ru>
2025-11-04 14:16:23 +0300

    SftoomiController fix

--- src/Controller/SftoomiController.php
+++ src/Controller/SftoomiController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Class\Core\DB\Connection as DBConnection;
+use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 abstract class SftoomiController extends AbstractController
 {
-    protected EntityManagerInterface $entityManager;
-    protected Connection $connection;
+    protected DBConnection $connection;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(DBConnection $connection)
     {
-        $this->entityManager = $entityManager;
-        $this->connection = $entityManager->getConnection();
+        $this->connection = $connection;
+    }
+
+    protected function assertAllRequiredFieldsSet(array $requiredFields, array $values): void
+    {
+        if (empty($requiredFields)) {
+            return;
+        }
+
+        if (empty($values)) {
+            throw new InvalidArgumentException(sprintf(
+                "Required fields for the operation: %s",
+                implode(", ", $requiredFields)
+            ));
+        }
+
+        foreach ($requiredFields as $field) {
+            if (empty($values[$field])) {
+                throw new InvalidArgumentException(sprintf(
+                    "Field %s is required",
+                    $field
+                ));
+            }
+        }
     }
 }
