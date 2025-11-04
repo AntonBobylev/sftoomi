6d0d55cd87bc672da679dead088eb68f103b2f7b
Anton Bobylev <ya-anton3632@yandex.ru>
2025-11-04 14:16:11 +0300

    garbage removed

--- src/EventSubscriber/ExceptionTransactionSubscriber.php
+++ src/EventSubscriber/ExceptionTransactionSubscriber.php
@@ -31,7 +31,6 @@ class ExceptionTransactionSubscriber implements EventSubscriberInterface
     {
         $this->connection->beginTransaction();
         $this->transactionStarted = true;
-        \App::logDump("here");
     }
 
     /**
