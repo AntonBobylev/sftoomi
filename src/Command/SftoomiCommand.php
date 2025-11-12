e05e249781e468ca531384eecd59f16806dcd263
Anton Bobylev <ya-anton3632@yandex.ru>
2025-11-12 19:23:00 +0300

    log method added to SftoomiCommand

--- src/Command/SftoomiCommand.php
+++ src/Command/SftoomiCommand.php
@@ -3,14 +3,18 @@
 namespace App\Command;
 
 use App\Class\Core\DB\Connection;
+use Psr\Log\LogLevel;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 abstract class SftoomiCommand extends Command
 {
-    public function __construct(protected readonly Connection $connection)
-    {
+    public function __construct(
+        protected readonly Connection $connection,
+        protected readonly Filesystem $filesystem
+    ) {
         parent::__construct();
     }
 
@@ -25,10 +29,21 @@ abstract class SftoomiCommand extends Command
     protected function createProgressBar(OutputInterface $output, int $count): ProgressBar
     {
         $progress = new ProgressBar($output, $count);
-        $progress->setBarCharacter('<fg=magenta>=</>');
+        $progress->setBarCharacter("<fg=magenta>=</>");
         $progress->setRedrawFrequency(floor($count / 100));
         $progress->start();
 
         return $progress;
+    }
+
+    protected function log(string $message, OutputInterface $output, string $level = LogLevel::INFO): void
+    {
+        $message = match($level) {
+            LogLevel::ERROR   => sprintf("<fg=red>%s</>", $message),
+            LogLevel::WARNING => sprintf("<fg=yellow>%s</>", $message),
+            default           => $message
+        };
+
+        $output->writeln($message);
     }
 }
