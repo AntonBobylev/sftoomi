ae4d57af77ab459472976e128d6d1f2efd7bd38a
Anton Bobylev <ya-anton3632@yandex.ru>
2026-01-11 19:32:07 +0300

    paths sorting

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -14,44 +14,44 @@ import UsersModuleComponent from './views/users/users.component';
 const BASE_TITLE = 'SFTOOMI :: ';
 
 export enum RoutesPaths {
-    HOME = '',
-    PATIENTS = 'patients',
-    REFERRING_FACILITIES = 'referring-facilities',
-    REFERRING_DOCTORS = 'referring-doctors',
-    STUDIES = 'studies',
     EXAMINATIONS = 'examinations',
+    HOME = '',
     LOGIN = 'login',
+    PATIENTS = 'patients',
+    REFERRING_DOCTORS = 'referring-doctors',
+    REFERRING_FACILITIES = 'referring-facilities',
+    STUDIES = 'studies',
     USERS = 'users'
 }
 
 export const routes: Routes = [{
+    path: RoutesPaths.EXAMINATIONS,
+    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.examinations'),
+    component: ExaminationsComponent
+}, {
     path: RoutesPaths.HOME,
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.home'),
     component: HomeComponent
+}, {
+    path: RoutesPaths.LOGIN,
+    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.login'),
+    component: LoginComponent
 }, {
     path: RoutesPaths.PATIENTS,
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.patients'),
     component: PatientsComponent
 }, {
-    path: RoutesPaths.REFERRING_FACILITIES,
-    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.setup.referring_facilities'),
-    component: ReferringFacilitiesComponent
-}, {
     path: RoutesPaths.REFERRING_DOCTORS,
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.setup.referring_doctors'),
     component: ReferringDoctorsComponent
 }, {
+    path: RoutesPaths.REFERRING_FACILITIES,
+    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.setup.referring_facilities'),
+    component: ReferringFacilitiesComponent
+}, {
     path: RoutesPaths.STUDIES,
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.setup.studies'),
     component: StudiesModuleComponent
-}, {
-    path: RoutesPaths.EXAMINATIONS,
-    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.examinations'),
-    component: ExaminationsComponent
-}, {
-    path: RoutesPaths.LOGIN,
-    title: BASE_TITLE + Sftoomi.Translator.translate('navigation.login'),
-    component: LoginComponent
 }, {
     path: RoutesPaths.USERS,
     title: BASE_TITLE + Sftoomi.Translator.translate('navigation.administration.users'),
