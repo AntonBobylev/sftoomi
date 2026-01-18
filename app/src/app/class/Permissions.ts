6a2d4ac66991ad742ad1a622c2ea0b4020e9fea7
Anton Bobylev <ya-anton3632@yandex.ru>
2026-01-18 17:09:25 +0300

    Permissions class added

---
+++ app/src/app/class/Permissions.ts
@@ -0,0 +1,28 @@
+export default class Permissions
+{
+    private readonly storageKey: string = 'permissions';
+
+    public set(permissions: string[]): void
+    {
+        sessionStorage.setItem(this.storageKey, permissions.join(','));
+    }
+
+    public get(): string[]
+    {
+        let permissions: string | null = sessionStorage.getItem(this.storageKey);
+
+        return permissions
+            ? permissions.split(',')
+            : [];
+    }
+
+    public isAllowed(permissionName: string): boolean
+    {
+        let permissions: string[] = this.get();
+        if (permissions.includes('*')) {
+            return true;
+        }
+
+        return this.get().includes(permissionName);
+    }
+}
