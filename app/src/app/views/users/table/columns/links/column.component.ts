6e27530492323f01f7f29697384791189c4a9598
Anton Bobylev <ya-anton3632@yandex.ru>
2026-07-18 23:46:14 +0300

    common column applied for users table links column

--- app/src/app/views/users/table/columns/links/column.component.ts
+++ app/src/app/views/users/table/columns/links/column.component.ts
@@ -1,22 +1,16 @@
-import { Component, Input } from '@angular/core';
-
-import Sftoomi from '../../../../../class/Sftoomi';
+import { Component } from '@angular/core';
 
 import UsersTableComponent from '../../table.component'
+import AppTableCommonColumn from '../../../../../components/core/app-table/common-column.component'
 
 import getUsersAPI from '../../../../../APIs/getUsersAPI'
 
 @Component({
     selector: 'users-table-links-column',
     templateUrl: './column.component.html',
-    styleUrl: './column.component.less',
-    imports: []
+    styleUrl: './column.component.less'
 })
 
-export default class LinksTablePermissionsColumnComponent
+export default class LinksTablePermissionsColumnComponent extends AppTableCommonColumn<getUsersAPI['data'][0], UsersTableComponent>
 {
-    @Input() public rowData!: getUsersAPI['data'][0];
-    @Input() public table!: UsersTableComponent;
-
-    protected readonly Sftoomi: typeof Sftoomi = Sftoomi;
 }
